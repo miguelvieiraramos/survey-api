b9c27a1786b04139e1073b124a12ec1cd1c5facd
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-08-12 22:52:20 -0300

    test: ensure SignUpController returns error if no name is provided

--- presentation/controllers/test_signup.py
+++ presentation/controllers/test_signup.py
@@ -12,3 +12,5 @@ def test_should_return_400_if_no_name_is_provided():
     }
     http_response = sut.handle(http_request)
     assert http_response.get('status_code') == 400
+    assert isinstance(http_response.get('body'), Exception)
+    assert http_response.get('body').args[0] == 'Missing param: name'
