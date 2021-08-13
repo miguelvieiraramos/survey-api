3aa5d3fc06686428976a66e2aba53b1d86d9d097
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-08-12 22:52:13 -0300

    feat: ensure SignUpController returns error if no name is provided

--- presentation/controllers/signup.py
+++ presentation/controllers/signup.py
@@ -2,5 +2,6 @@ class SignUpController:
 
     def handle(self, http_request: any) -> any:
         return {
-            'status_code': 400
+            'status_code': 400,
+            'body': Exception('Missing param: name')
         }
