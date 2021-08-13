61f3dfa1bf291a168c251815509315463ee49687
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-08-12 22:30:25 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ presentation/controllers/test_signup.py
@@ -0,0 +1,14 @@
+from presentation.controllers.signup import SignUpController
+
+
+def test_should_return_400_if_no_name_is_provided():
+    sut = SignUpController()
+    http_request = {
+        'body': {
+            'email': 'any_email',
+            'password': 'any_password',
+            'password_confirmation': 'any_password',
+        }
+    }
+    http_response = sut.handle(http_request)
+    assert http_response.get('status_code') == 400
