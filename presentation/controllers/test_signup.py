2ca1f0f76cdf94e930f5d6009e261537828ec716
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-08-14 23:37:52 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- presentation/controllers/test_signup.py
+++ presentation/controllers/test_signup.py
@@ -30,3 +30,18 @@ def test_should_return_400_if_no_email_is_provided():
     assert http_response.status_code == 400
     assert isinstance(http_response.body, MissingParamError)
     assert http_response.body.args[0] == 'Missing param: email'
+
+
+def test_should_return_400_if_no_password_is_provided():
+    sut = SignUpController()
+    http_request = {
+        'body': {
+            'name': 'any_name',
+            'email': 'any_email',
+            'password_confirmation': 'any_password',
+        }
+    }
+    http_response = sut.handle(http_request)
+    assert http_response.status_code == 400
+    assert isinstance(http_response.body, MissingParamError)
+    assert http_response.body.args[0] == 'Missing param: password'
