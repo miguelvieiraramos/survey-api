01adf026190b7bd4e60706af1f7dedfc1fc378c4
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-09-15 20:56:55 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- utils/test_email_validator_adapter.py
+++ utils/test_email_validator_adapter.py
@@ -1,7 +1,16 @@
+from unittest.mock import patch, Mock
+
 from utils.email_validator_adapter import EmailValidatorAdapter
 
 
+@patch('utils.email_validator_adapter.validators.email', Mock(return_value=False))
 def test_should_return_false_if_validator_returns_false():
     sut = EmailValidatorAdapter()
     is_valid = sut.is_valid('invalid_email@gmail.com')
     assert is_valid == False
+
+
+def test_should_return_true_if_validator_returns_true():
+    sut = EmailValidatorAdapter()
+    is_valid = sut.is_valid('valid_email@gmail.com')
+    assert is_valid == True
