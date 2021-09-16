10052067787bf8a1111c0944a050f486de7a5dd2
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-09-15 21:01:21 -0300

    refactor: move sut creation to a factory method

--- utils/test_email_validator_adapter.py
+++ utils/test_email_validator_adapter.py
@@ -3,22 +3,26 @@ from unittest.mock import patch, Mock
 from utils.email_validator_adapter import EmailValidatorAdapter
 
 
+def make_sut() -> EmailValidatorAdapter:
+    return EmailValidatorAdapter()
+
+
 @patch('utils.email_validator_adapter.validators.email', Mock(return_value=False))
 def test_should_return_false_if_validator_returns_false():
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     is_valid = sut.is_valid('invalid_email@gmail.com')
     assert is_valid == False
 
 
 @patch('utils.email_validator_adapter.validators.email', Mock(return_value=True))
 def test_should_return_true_if_validator_returns_true():
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     is_valid = sut.is_valid('valid_email@gmail.com')
     assert is_valid == True
 
 
 @patch('utils.email_validator_adapter.validators.email')
 def test_should_call_validator_with_correct_email(email_validator_mock):
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     sut.is_valid('any_email@gmail.com')
     email_validator_mock.assert_called_once_with('any_email@gmail.com')
