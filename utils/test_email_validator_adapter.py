8fbb0ec2fe8d75e22f2d4c72e1efcb4be7005815
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-09-15 21:00:03 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- utils/test_email_validator_adapter.py
+++ utils/test_email_validator_adapter.py
@@ -10,7 +10,15 @@ def test_should_return_false_if_validator_returns_false():
     assert is_valid == False
 
 
+@patch('utils.email_validator_adapter.validators.email', Mock(return_value=True))
 def test_should_return_true_if_validator_returns_true():
     sut = EmailValidatorAdapter()
     is_valid = sut.is_valid('valid_email@gmail.com')
     assert is_valid == True
+
+
+@patch('utils.email_validator_adapter.validators.email')
+def test_should_call_validator_with_correct_email(email_validator_mock):
+    sut = EmailValidatorAdapter()
+    sut.is_valid('any_email@gmail.com')
+    email_validator_mock.assert_called_once_with('any_email@gmail.com')
