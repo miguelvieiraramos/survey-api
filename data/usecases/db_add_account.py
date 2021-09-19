6585a9ea18d1a65dccc053dd04201ca9655da374
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-09-19 19:39:10 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with corrects params

--- data/usecases/db_add_account.py
+++ data/usecases/db_add_account.py
@@ -1,3 +1,4 @@
+from data.protocols.add_account_repository import AddAccountRepository
 from data.protocols.encrypter import Encrypter
 from domain.models.account import AccountModel
 from domain.usecases.add_account import AddAccountModel, AddAccount
@@ -5,8 +6,12 @@ from domain.usecases.add_account import AddAccountModel, AddAccount
 
 class DbAddAccount(AddAccount):
 
-    def __init__(self, encrypter: Encrypter):
+    def __init__(self, encrypter: Encrypter, add_account_repository: AddAccountRepository):
         self.encrypter = encrypter
+        self.add_account_repository = add_account_repository
 
-    def add(self, account: AddAccountModel) -> AccountModel:
-        self.encrypter.encrypt(account.password)
+    def add(self, account_data: AddAccountModel) -> AccountModel:
+        hashed_password = self.encrypter.encrypt(account_data.password)
+        self.add_account_repository.add(
+            AddAccountModel(name=account_data.name, email=account_data.email, password=hashed_password)
+        )
