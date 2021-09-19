5f5a1d55fc7a86ec30ab87fdb02006f0b8a5e5fd
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-09-19 19:39:14 -0300

    test: ensure DbAddAccount calls AddAccountRepository with corrects params

--- data/usecases/test_db_add_account.py
+++ data/usecases/test_db_add_account.py
@@ -2,23 +2,38 @@ from unittest.mock import Mock
 
 import pytest
 
+from data.protocols.add_account_repository import AddAccountRepository
+from data.protocols.encrypter import Encrypter
 from data.usecases.db_add_account import DbAddAccount
+from domain.models.account import AccountModel
 from domain.usecases.add_account import AddAccountModel
 
 
-class EncrypterStub:
+class EncrypterStub(Encrypter):
 
     def encrypt(self, value: str) -> str:
         return 'hashed_password'
 
 
+class AddAccountRepositoryStub(AddAccountRepository):
+
+    def add(self, account_data: AddAccountModel) -> AccountModel:
+        return AccountModel(
+            id='valid_id',
+            name='valid_name',
+            email='valid_email',
+            password='valid_password'
+        )
+
+
 def make_sut():
     encrypter_stub = EncrypterStub()
-    return DbAddAccount(encrypter_stub), encrypter_stub
+    add_account_repository_stub = AddAccountRepositoryStub()
+    return DbAddAccount(encrypter_stub, add_account_repository_stub), encrypter_stub, add_account_repository_stub
 
 
 def test_should_call_encrypter_with_correct_password():
-    sut, encrypter_stub = make_sut()
+    sut, encrypter_stub, _ = make_sut()
     encrypter_stub.encrypt = Mock()
     account_data = AddAccountModel(
         name='valid_name',
@@ -30,7 +45,7 @@ def test_should_call_encrypter_with_correct_password():
 
 
 def test_should_raise_if_encrypter_raises():
-    sut, encrypter_stub = make_sut()
+    sut, encrypter_stub, _ = make_sut()
     encrypter_stub.encrypt = Mock()
     encrypter_stub.encrypt.side_effect = Exception
     account_data = AddAccountModel(
@@ -41,3 +56,17 @@ def test_should_raise_if_encrypter_raises():
 
     with pytest.raises(Exception):
         sut.add(account_data)
+
+
+def test_should_call_AddAccountRepository_with_correct_params():
+    sut, _, add_account_repository_stub = make_sut()
+    add_account_repository_stub.add = Mock()
+    account_data = AddAccountModel(
+        name='valid_name',
+        email='valid_email',
+        password='valid_password'
+    )
+    sut.add(account_data)
+    add_account_repository_stub.add.assert_called_once_with(
+        AddAccountModel(name='valid_name',  email='valid_email', password='hashed_password')
+    )
