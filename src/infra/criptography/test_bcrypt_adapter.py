46236e29061fd04adc62e0c13d522c7506ce7722
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-09-27 21:41:45 -0300

    test: ensure BcryptAdapter returns hash on success

--- src/infra/criptography/test_bcrypt_adapter.py
+++ src/infra/criptography/test_bcrypt_adapter.py
@@ -1,11 +1,22 @@
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from src.infra.criptography.bcrypt_adapter import BcryptAdapter
 
 
-@patch('src.infra.criptography.bcrypt_adapter.bcrypt.hashpw')
+@patch('src.infra.criptography.bcrypt_adapter.bcrypt')
 def test_should_call_bcrypt_with_correct_values(mock_bcrypt):
-    salt = 12
-    sut = BcryptAdapter(salt)
+    sut = BcryptAdapter()
+    mock_bcrypt.gensalt = Mock(return_value='salt')
+
     sut.encrypt('any_value')
-    mock_bcrypt.assert_called_once_with(b'any_value', 12)
+    mock_bcrypt.hashpw.assert_called_once_with(b'any_value', 'salt')
+
+
+@patch('src.infra.criptography.bcrypt_adapter.bcrypt')
+def test_should_return_hash_on_success(mock_bcrypt):
+    sut = BcryptAdapter()
+    mock_bcrypt.hashpw = Mock(return_value='hash')
+
+    hash = sut.encrypt('any_value')
+
+    assert hash == 'hash'
