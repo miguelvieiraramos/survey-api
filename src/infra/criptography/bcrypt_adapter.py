a07da7e7be9400eaf5c2089a4c438e6969ba8d5e
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-09-27 21:07:36 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/criptography/bcrypt_adapter.py
@@ -0,0 +1,12 @@
+import bcrypt
+
+from src.data.protocols.encrypter import Encrypter
+
+
+class BcryptAdapter(Encrypter):
+
+    def __init__(self, salt: int):
+        self.salt = salt
+
+    def encrypt(self, value: str) -> str:
+        bcrypt.hashpw(value, self.salt)
